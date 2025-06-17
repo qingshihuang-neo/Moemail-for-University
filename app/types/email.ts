c86d2901dfd5b4ace7856dcdfdf5ccb2593aba3e
qingshihuang-neo <qingshihuangv50@gmail.com>
2025-06-17 11:10:38 +0800

    Update email.ts

--- app/types/email.ts
+++ app/types/email.ts
@@ -4,8 +4,8 @@ export interface ExpiryOption {
 }
 
 export const EXPIRY_OPTIONS: ExpiryOption[] = [
-  { label: '永久', value: 0 },
-  { label: '永久', value: 0 },
-  { label: '永久', value: 0 },
+  { label: '1小时', value: 1000 * 60 * 60 },
+  { label: '24小时', value: 1000 * 60 * 60 * 24 },
+  { label: '3天', value: 1000 * 60 * 60 * 24 * 3 },
   { label: '永久', value: 0 }
 ]
